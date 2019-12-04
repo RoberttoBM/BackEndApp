ff022df9ee7aeb164c7c680900d431c8e0ebd380
Victor Sandoval <victor.sandoval@vallegrande.edu.pe>
2019-12-04 11:52:08 -0500

    Local :D

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,9 +10,12 @@ import { HelpModule } from './help/help.module';
 @Module({
   imports: [TypeOrmModule.forRoot({
     "type": "oracle",
-    "username": "dbEdooca",
-    "password": "Edooca-2019",
-    "connectString": "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=35.184.25.222)(PORT=1521))(CONNECT_DATA=(SID=XE)))",
+    //"username": "dbEdooca", nube
+    //"password": "Edooca-2019", nube
+    "username":"EdoocaL",
+    "password":"123",
+    //"connectString": "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=35.184.25.222)(PORT=1521))(CONNECT_DATA=(SID=XE)))", nube
+    "connectString": "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521))(CONNECT_DATA=(SID=XE)))",
     "synchronize": false,
     "logging": true,
     "entities": [__dirname + '/**/**.entity{.ts,.js}']
